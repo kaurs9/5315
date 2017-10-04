654663aaa772060362f8610f78efd46f7ebc58d3
kaurs9 <32150532+kaurs9@users.noreply.github.com>
2017-10-03 22:18:19 -0700

    hw1 commit2

---
+++ custdata.js
@@ -0,0 +1,124 @@
+{/Users/Sunita/Downloads/hw1source 2/jsontable.html
+  "data": [
+    {
+      "name": “Jane Doe“,
+      "job title": “Rocket Scientist”,
+      "phone number": “435-343-2322”,
+      "last_purchase_date": "2010/01/01”,
+      "location": “Istanbul”,
+      "cust_id": "8400”
+    },
+    {
+      "name": “John Smith“,
+      "job title": “Data Analyst“,
+      "phone number": “123-232-4212”,
+      "last_purchase_date": "2010/01/02",
+      "location": "New York",
+      "cust_id": "4800”
+    },
+    {
+      "name": “Kate Fox“,
+      "job title": "Author",
+      "phone number": “892-234-1232”,
+      "last_purchase_date": "2010/01/12",
+      "location": “San Diego“,
+      "cust_id": "1500”
+    },
+    {
+      "name": “Har Sing”,
+      "job title": “Engineer”,
+      "phone number": "$552-334-5234”,
+      "last_purchase_date": "2010/10/28",
+      "location": “Delhi”,
+      "cust_id": "5002”
+    },
+    {
+      "name": “Bo Johnson“,
+      "job title": “Tech Support“,
+      "phone number": “132-324-6423”,
+      "last_purchase_date": "2011/02/12",
+      "location": “Dallas”,
+      "cust_id": "8000”
+    },
+    {
+      "name": “Chad Miller“,
+      "job title": “Medical Doctor“,
+      "phone number": “765-543-2346”,
+      "last_purchase_date": "2003/01/13",
+      "location": "Edinburgh",
+      "cust_id": “9000”
+    },
+    {
+      "name": “Mindy Knot”,
+      "job title": “Software Developer”,
+      "phone number": “903-324-5664”,
+      "last_purchase_date": "2009/03/29",
+      "location": “Dallas”,
+      "cust_id": "6990”
+    },
+    {
+      "name": “Bill Clinton“,
+      "job title": “Consultant”,
+      "phone number": “654-234-7743”,
+      "last_purchase_date": "2000/12/18",
+      "location": “Washington DC”,
+      "cust_id": "3500”
+    },
+    {
+      "name": "Colleen Hoover”,
+      "job title": “Author”,
+      "phone number": “543-664-6423”,
+      "last_purchase_date": "2014/07/15",
+      "location": “Salem”,
+      "cust_id": "2309”
+    },
+    {
+      "name": “Zara Malik”,
+      "job title": “Product Manager”,
+      "phone number": “928-348-2323”,
+      "last_purchase_date": "2014/09/26",
+      "location": “London”,
+      "cust_id": “3293”
+    },
+    {
+      "name": “Danny Sharma”,
+      "job title": “Fireman”,
+      "phone number": “895-324-3423“,
+      "last_purchase_date": "2012/02/24”,
+      "location": "Seattle”,
+      "cust_id": "9608"
+    },
+    {
+      "name": “Nara Snub”,
+      "job title": “Data Specialist",
+      "phone number": “903-324-4322”,
+      "last_purchase_date": "2014/11/14",
+      "location": “Maimi”,
+      "cust_id": "6200"
+    },
+    {
+      "name": "Sonia Wellton“,
+      "job title": “Electrical Engineer",
+      "phone number": “032-234-1342”,
+      "last_purchase_date": "2012/12/13",
+      "location": "Edinburgh",
+      "cust_id": "1687"
+    },
+    {
+      "name": “Bob Barker“,
+      "job title": “Host”,
+      "phone number": “882-238-2342”,
+      "last_purchase_date": “1999/12/19",
+      "location": “Hollywood”,
+      "cust_id": “0023”
+    },
+    {
+      "name": “Sam Tyler”,
+      "job title": “Team Lead",
+      "phone number": “809-234-2399”,
+      "last_purchase_date": "2013/03/03",
+      "location": “Seattle”,
+      "cust_id": "9493”
+    }
+  ]
+}
