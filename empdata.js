82719b60e3400709c07d366e15977a1ec74cfe82
kaurs9 <32150532+kaurs9@users.noreply.github.com>
2017-10-04 16:49:20 -0700

    hw1 commit11

--- empdata.js
+++ empdata.js
@@ -23,6 +23,30 @@
       "purchase_date": "2005/01/12",
       "location": "San Francisco",
       "cust_id": "0013"
+    },
+     {
+      "name": "Bill Johnson",
+      "occupation": "Salesman",
+      "sold_price": "$30,900",
+      "purchase_date": "2011/07/23",
+      "location": "Dallas",
+      "cust_id": "0014"
+    },
+    {
+      "name": "Kelly Kapp",
+      "occupation": "Sales Manager",
+      "sold_price": "90,030",
+      "purchase_date": "2003/05/02",
+      "location": "Delhi",
+      "cust_id": "0015"
+    },
+    {
+      "name": "Dan Harten",
+      "occupation": "Educator",
+      "sold_price": "$100,000",
+      "purchase_date": "2015/11/12",
+      "location": "San Francisco",
+      "cust_id": "0016"
     }
   ]
 }
